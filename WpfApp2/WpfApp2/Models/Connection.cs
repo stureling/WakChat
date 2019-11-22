282ee0ddbd87835a5429c37a6c02c6ed0ba17d9d
annli504 <annli504@student.liu.se>
2019-11-22 14:08:58 +0100

    git <3

---
+++ WpfApp2/WpfApp2/Models/Connection.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using System.Windows;
+using WpfApp2.Models;
+
+namespace WpfApp2.Viewmodels
+{
+    public class Connection
+    {
+        public Connection()
+        {
+            Client = new TcpClient();
+        }
+
+        public TcpClient Client { get; set; }
+
+        public TcpListener Server { get; set; }
+    }
+}
