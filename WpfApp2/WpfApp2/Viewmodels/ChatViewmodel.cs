ff57d5028457d873c54ef9cd39aea2f71161a9dc
annli504 <annli504@student.liu.se>
2019-11-27 14:20:26 +0100

    ashdaoio

--- WpfApp2/WpfApp2/Viewmodels/ChatViewmodel.cs
+++ WpfApp2/WpfApp2/Viewmodels/ChatViewmodel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
@@ -12,20 +13,34 @@ using WpfApp2.Views;
 
 namespace WpfApp2.Viewmodels
 {
-    public class ChatViewmodel : BaseViewmodel
+    public class ChatViewmodel : BaseViewmodel, INotifyPropertyChanged
     {
         private Connection connection;
+        public User User { get; set; }
+        public string _msg;
         public ICommand ExitWindowCommand { get; set; }
         public ICommand OpenWindowCommand { get; set; }
+        public ICommand SendCommand { get; set; }
 
-        public ChatViewmodel(Connection connection)
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ChatViewmodel(Connection connection, User user)
         {
             this.ExitWindowCommand = new ExitWindowCommand(this);
             this.OpenWindowCommand = new OpenWindowCommand(this);
             this.connection = connection;
+            this.User = user;
         }
 
+        public void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
 
-        public Message Msg { get; set; }
+        public void SendMessage()
+        {
+            connection.Send(User, _msg);
+        }
     }
 }
