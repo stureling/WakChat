c72dd83dbde840d453c4c5b86df2ad9e95021653
Eric <nope@nope.nope>
2019-11-27 14:06:00 +0100

    moved connection-functions to the connection model

--- WpfApp2/WpfApp2/Models/Connection.cs
+++ WpfApp2/WpfApp2/Models/Connection.cs
@@ -7,6 +7,7 @@ using System.Net.Sockets;
 using System.Text;
 using System.Text.Json;
 using System.Text.Json.Serialization;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using WpfApp2.Models;
@@ -19,9 +20,180 @@ namespace WpfApp2.Viewmodels
         {
             Client = new TcpClient();
         }
-
+        private Thread connectionThread;
         public TcpClient Client { get; set; }
 
         public TcpListener Server { get; set; }
-    }
+        public void Connect(User user)
+        {
+            if(connectionThread != null)
+            {
+                Debug.WriteLine("Thread aborted to make room for new thread");
+                connectionThread.Abort();
+                Debug.WriteLine("Thread aborted");
+            }
+            connectionThread = new Thread(() => ConnectThread(user));
+            connectionThread.Start();
+        }
+        
+        public void Listen(User user)
+        {
+            if(connectionThread != null)
+            {
+                Debug.WriteLine("Starting thread abortion to make room for new thread");
+                connectionThread.Abort();
+                Debug.WriteLine("Thread aborted");
+            }
+            connectionThread = new Thread(() => ListenThread(user));
+            connectionThread.Start();
+        }
+        
+        private void ConnectThread(User user)
+        {
+            try
+            {
+                IPAddress serverIP = IPAddress.Parse(user.IP);
+                Client.Connect(serverIP, user.Port);
+
+                ConnectionJSON json = new ConnectionJSON(user.Username, "EstablishConnection", "request");
+                string jsonString = JsonSerializer.Serialize(json);
+
+                // Encode the string to bytearray
+                byte[] data = Encoding.UTF8.GetBytes(jsonString);
+
+                // Get a client stream for reading and writing.
+                NetworkStream stream = Client.GetStream();
+
+                //// Send the message to the connected TcpServer. 
+                stream.Write(data, 0, data.Length);
+
+                // Buffer to store the response bytes.
+                data = new Byte[256];
+
+                // String to store the response UTF8 representation.
+                String responseData = String.Empty;
+
+                //// Read the first batch of the TcpServer response bytes.
+                Int32 bytes = stream.Read(data, 0, data.Length);
+                responseData = System.Text.Encoding.UTF8.GetString(data, 0, bytes);
+                ConnectionJSON responseJson = JsonSerializer.Deserialize<ConnectionJSON>(responseData);
+                Console.WriteLine($"Received: {responseData}");
+
+                if (responseJson.ConnectionTypeValue == "Deny")
+                {
+                    // go back to connection window if denied
+                    MessageBox.Show("Connection denied by host", "Alert", MessageBoxButton.OK);
+                    Client.Close();
+                }
+                else if (responseJson.ConnectionTypeValue == "Accept")
+                {
+                    // continue to next window
+                    //StartChat();
+                    MessageBox.Show("Connection accepted by host, continue to chat window", "Alert", MessageBoxButton.OK);
+                }
+                
+                else
+                {
+                    Debug.WriteLine("wierd value");
+                }
+            }
+            catch (ArgumentNullException e)
+            {
+                Console.WriteLine("ArgumentNullException: {0}", e);
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("SocketException: {0}", e);
+                MessageBox.Show("No host listening on port-IP combination", "Alert", MessageBoxButton.OK);
+                //Tell the user that the connection was refused. Probably due to no host listening on the provided port and IP combination.
+            }
+            catch(ThreadAbortException e)
+            {
+                Debug.WriteLine(e);
+            }
+        }
+
+        private void ListenThread(User user)
+        {
+            try
+            {
+                IPAddress localAddr = IPAddress.Parse("127.0.0.1");
+                
+                // TcpListener server = new TcpListener(port);
+                Server = new TcpListener(localAddr, user.Port);
+                
+                // Start listening for client requests.
+                Server.Start();
+                
+                // Buffer for reading data
+                Byte[] bytes = new Byte[256];
+                String data = null;
+
+                Debug.Write("Waiting for a connection... ");
+
+                // Enter the listening loop.
+                while (true)
+                {
+                    if (Server.Pending())
+                    {
+                        Client = Server.AcceptTcpClient();
+                        
+                        data = null;
+
+                        // Get a stream object for reading and writing
+                        NetworkStream stream = Client.GetStream();
+
+                        int i;
+                        
+                        i = stream.Read(bytes, 0, bytes.Length);
+
+                        
+                        data = Encoding.UTF8.GetString(bytes, 0, i);
+                        Debug.WriteLine($"Received: {data}");
+                        ConnectionJSON jsonData = JsonSerializer.Deserialize<ConnectionJSON>(data);
+
+                        // Creates message box to inform user of connected 
+                        MessageBoxResult result = MessageBox.Show($"User {jsonData.Username} wishes to connect, Accept?", "Alert", MessageBoxButton.YesNo);
+                        
+                        if (result == MessageBoxResult.Yes)
+                        {
+                            // Send back a response.                        
+                            ConnectionJSON json = new ConnectionJSON(user.Username, "EstablishConnection", "Accept");
+                            string response = JsonSerializer.Serialize(json);
+                            byte[] msg = Encoding.UTF8.GetBytes(response);
+                            stream.Write(msg, 0, msg.Length);
+                            Debug.WriteLine($"Sent: {data}");
+                            break;
+                        }
+                        
+                        else if (result == MessageBoxResult.No)
+                        {
+                            // Send back a response.                        
+                            ConnectionJSON json = new ConnectionJSON(user.Username, "EstablishConnection", "Deny");
+                            string response = JsonSerializer.Serialize(json);
+                            byte[] msg = Encoding.UTF8.GetBytes(response);
+                            stream.Write(msg, 0, msg.Length);
+                            Debug.WriteLine($"Sent: {data}");
+                            
+                            Client.Close();
+                            Debug.Write("Waiting for a connection... ");
+                        }
+                    }
+                }
+                MessageBox.Show("Connection made, continue to chat window", "Alert", MessageBoxButton.OK);
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("SocketException: {0}", e);
+            }
+            catch(ThreadAbortException e)
+            {
+                Debug.WriteLine(e);
+            }
+            finally
+            {
+                Server.Stop();
+            }
+        }   
+    }    
 }
--- WpfApp2/WpfApp2/Viewmodels/LoginViewmodel.cs
+++ WpfApp2/WpfApp2/Viewmodels/LoginViewmodel.cs
@@ -21,7 +21,6 @@ namespace WpfApp2.Viewmodels
 {
     public class LoginViewmodel : BaseViewmodel, INotifyPropertyChanged
     {
-        private Thread connectionThread;
         public User User { get; set; }
         public Connection Connection { get; private set; }
 
@@ -39,176 +38,17 @@ namespace WpfApp2.Viewmodels
             this.ListenCommand = new ListenCommand(this);
             this.ConnectCommand = new ConnectCommand(this);
             this.User = new User();
+            this.Connection = new Connection();
         }
 
         public void Connect()
         {
-            if(connectionThread != null)
-            {
-                Debug.WriteLine("Thread aborted to make room for new thread");
-                //Freezes UI because abort is waiting for TcpClient _client = Server.AcceptTcpClient(); to finish in the threadfunction
-                connectionThread.Abort();
-                Debug.WriteLine("Thread aborted");
-            }
-            connectionThread = new Thread(new ThreadStart(ConnectThread));
-            connectionThread.Start();
+            Connection.Connect(User);
         }
+
         public void Listen()
         {
-            if(connectionThread != null)
-            {
-                Debug.WriteLine("Starting thread abortion to make room for new thread");
-                //Freezes UI because abort is waiting for TcpClient _client = Server.AcceptTcpClient(); to finish in the threadfunction
-                connectionThread.Abort();
-                Debug.WriteLine("Thread aborted");
-            }
-            connectionThread = new Thread(new ThreadStart(ListenThread));
-            connectionThread.Start();
-        }
-        public void ConnectThread()
-        {
-            try
-            {
-                Connection = new Connection();
-                IPAddress serverIP = IPAddress.Parse(User.IP);
-                Connection.Client.Connect(serverIP, User.Port);
-
-                ConnectionJSON json = new ConnectionJSON(User.Username, "EstablishConnection", "request");
-
-                string jsonString = JsonSerializer.Serialize(json);
-
-                // Encode the string to bytearray
-                byte[] data = Encoding.UTF8.GetBytes(jsonString);
-
-                // Get a client stream for reading and writing.
-                NetworkStream stream = Connection.Client.GetStream();
-
-                //// Send the message to the connected TcpServer. 
-                stream.Write(data, 0, data.Length);
-
-                // Buffer to store the response bytes.
-                data = new Byte[256];
-
-                // String to store the response UTF8 representation.
-                String responseData = String.Empty;
-
-                //// Read the first batch of the TcpServer response bytes.
-                Int32 bytes = stream.Read(data, 0, data.Length);
-                responseData = System.Text.Encoding.UTF8.GetString(data, 0, bytes);
-                ConnectionJSON responseJson = JsonSerializer.Deserialize<ConnectionJSON>(responseData);
-                Console.WriteLine($"Received: {responseData}");
-                if (responseJson.ConnectionTypeValue == "Deny")
-                {
-                    // go back to connection window if denied
-                    MessageBox.Show("Connection denied by host", "Alert", MessageBoxButton.OK);
-                    Connection.Client.Close();
-                }
-                else if (responseJson.ConnectionTypeValue == "Accept")
-                {
-                    // continue to next window
-                    //StartChat();
-                    MessageBox.Show("Connection accepted by host, continue to chat window", "Alert", MessageBoxButton.OK);
-                }
-                else
-                {
-                    Debug.WriteLine("wierd value");
-                }
-            }
-            catch (ArgumentNullException e)
-            {
-                Console.WriteLine("ArgumentNullException: {0}", e);
-            }
-            catch (SocketException e)
-            {
-                Console.WriteLine("SocketException: {0}", e);
-                MessageBox.Show("No host listening on port-IP combination", "Alert", MessageBoxButton.OK);
-                //Tell the user that the connection was refused. Probably due to no host listening on the provided port and IP combination.
-            }
-            catch(ThreadAbortException e)
-            {
-                Debug.WriteLine(e);
-            }
-        }
-        public void ListenThread()
-        {
-            try
-            {
-                Connection = new Connection();
-
-                IPAddress localAddr = IPAddress.Parse("127.0.0.1");
-
-                // TcpListener server = new TcpListener(port);
-                Connection.Server = new TcpListener(localAddr, User.Port);
-
-                // Start listening for client requests.
-                Connection.Server.Start();
-
-                // Buffer for reading data
-                Byte[] bytes = new Byte[256];
-                String data = null;
-
-                Debug.Write("Waiting for a connection... ");
-                // Enter the listening loop.
-                while (true)
-                {
-                    if (Connection.Server.Pending())
-                    {
-                        Connection.Client = Connection.Server.AcceptTcpClient();
-
-                        data = null;
-
-                        // Get a stream object for reading and writing
-                        NetworkStream stream = Connection.Client.GetStream();
-
-                        int i;
-                        i = stream.Read(bytes, 0, bytes.Length);
-
-                        data = Encoding.UTF8.GetString(bytes, 0, i);
-                        Debug.WriteLine($"Received: {data}");
-                        ConnectionJSON jsonData = JsonSerializer.Deserialize<ConnectionJSON>(data);
-
-                        // Creates message box to inform user of connected 
-                        MessageBoxResult result = MessageBox.Show($"User {jsonData.Username} wishes to connect, Accept?", "Alert", MessageBoxButton.YesNo);
-
-                        if (result == MessageBoxResult.Yes)
-                        {
-                            // Send back a response.                        
-                            ConnectionJSON json = new ConnectionJSON(User.Username, "EstablishConnection", "Accept");
-                            string response = JsonSerializer.Serialize(json);
-                            byte[] msg = Encoding.UTF8.GetBytes(response);
-                            stream.Write(msg, 0, msg.Length);
-                            Debug.WriteLine($"Sent: {data}");
-                            break;
-                        }
-                        else if (result == MessageBoxResult.No)
-                        {
-                            // Send back a response.                        
-                            ConnectionJSON json = new ConnectionJSON(User.Username, "EstablishConnection", "Deny");
-                            string response = JsonSerializer.Serialize(json);
-                            byte[] msg = Encoding.UTF8.GetBytes(response);
-                            stream.Write(msg, 0, msg.Length);
-                            Debug.WriteLine($"Sent: {data}");
-                            Connection.Client.Close();
-                            Debug.Write("Waiting for a connection... ");
-                        }
-                    }
-               }
-                //Connection has been made
-                //StartChat();
-                MessageBox.Show("Connection made, continue to chat window", "Alert", MessageBoxButton.OK);
-            }
-            catch (SocketException e)
-            {
-                Console.WriteLine("SocketException: {0}", e);
-            }
-            catch(ThreadAbortException e)
-            {
-                Debug.WriteLine(e);
-            }
-            finally
-            {
-                Connection.Server.Stop();
-            }
+            Connection.Listen(User);
         }
 
         public void StartChat()
