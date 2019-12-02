90714c5f97b42340e5c8573f46888bdd1dddedb7
annli504 <annli504@student.liu.se>
2019-12-02 16:32:02 +0100

    Pls dont kill me with this merge

---
+++ WpfApp2/WpfApp2/Models/Message.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp2.Models
+{
+    public class Message: INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public Message(){}
+        public string Username { get; set; }
+        public DateTime Time { get; set; }
+        public string Msg { get; set; }
+
+        public void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
